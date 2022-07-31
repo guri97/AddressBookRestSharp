499f177fbcec8f25f8001c160c32979c258db8b1
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-31 20:40:47 +0530

    [gurpreet] UC1_ReadEntiries

---
+++ AddressBookRestSharp/AddressBookRestSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddressBookRestSharp
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Address Book Using RestSharp!");
+        }
+    }
+}
+
